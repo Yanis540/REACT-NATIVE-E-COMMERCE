856d7041a5fe111c94d12593d2a078860b9f6e80
Yanis540 <yanistabellout4@gmail.com>
2023-07-13 03:44:45 +0100

    address

--- frontend/src/routes/Stacks/Home/Header/HomeHeader.tsx
+++ frontend/src/routes/Stacks/Home/Header/HomeHeader.tsx
@@ -3,15 +3,17 @@ import { Text, View ,TouchableOpacity } from 'react-native'
 import { useHomeNavigation } from '../../..';
 import {Dimensions} from 'react-native';
 import { Ionicons , AntDesign , Entypo } from '@expo/vector-icons'; 
+import { useAuth } from '../../../../context/store';
 function HomeHeader() {
     const ScreenWidth = Dimensions.get('window').width;
+    const {user} = useAuth(); 
     const {navigation,route} = useHomeNavigation(); 
     return (
         <View style={{width:ScreenWidth-20}} className="mt-[40px]  flex-1 flex  flex-row items-center gap-[5px] w-screen h-full ">
             {
                 route?.name == "HomeScreen" ? (
 
-                    <Text className='flex-1 font-bold text-emerald-400 text-2xl'>Yanis</Text>
+                    <Text className='flex-1 font-bold text-emerald-400 text-2xl'>{user?.name??"welcome"}</Text>
                 ):(
                     <TouchableOpacity onPress={()=>navigation.goBack()} className="">
                         <AntDesign name="arrowleft" size={24} color="rgba(52, 211,153,1)"  />
--- frontend/src/screen/Basket/Basket.tsx
+++ frontend/src/screen/Basket/Basket.tsx
@@ -1,4 +1,4 @@
-import { useBasket } from '../../context/store';
+import { useAuth, useBasket } from '../../context/store';
 import React from 'react';
 import { Text, View  , Image, ScrollView} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
@@ -11,9 +11,6 @@ import KeyboardLayout from '../../Layout/KeyboardLayout';
 
 function Basket() {
     const {basket} = useBasket();
-    const imgUsr = "https://lh3.googleusercontent.com/ogw/AGvuzYat7F1A7-_s8o_zpIRmvVIgHVm44YKG0-DxBWs8=s32-c-mo"
-    const user: User={name:"yanis tabellout",email:"yanis@gmail.com",created_at:"2022",id:"1",orders:[],favorite_products:[],image:imgUsr}
-    const defaultImgUsr = "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
     
     if(!basket?.length) return (
         <View className="flex-1 flex flex-col items-center justify-center gap-y-[20px] bg-white ">
